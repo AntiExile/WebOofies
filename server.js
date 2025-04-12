5e09210e3a9bbb813e8ad75d56252763ac54962e
AntiExile <wianmarais122007@gmail.com>
2025-04-12 05:20:22 +0200

    7

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
+
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie: true });
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(csrfProtection);
+app.get('/', (req, res) => {
+  res.render('index', { csrfToken: req.csrfToken() });
+});
+app.post('/process', (req, res) => {
+  res.send('Form processed successfully!');
+});
 
 app.use(express.static(path.join(__dirname)));
 
